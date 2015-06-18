03a3ae706f92738b40dcc99e365e1d2c04b4298e
yaoshimax <yaoshimax@gmail.com>
2015-06-19 00:29:58 +0900

    初日真っ先にCOする霊媒師。 Sample相手だと翌日に狂人・狼が霊能COして吊られることが多く、勝率は25%程度の模様。

---
+++ src/org/aiwolf/firstAgent/FirstCOMedium.java
@@ -0,0 +1,47 @@
+package org.aiwolf.firstAgent;
+
+import java.util.ArrayList;
+
+import org.aiwolf.client.base.player.AbstractMedium;
+import org.aiwolf.client.lib.TemplateTalkFactory;
+import org.aiwolf.common.data.Agent;
+import org.aiwolf.common.data.Judge;
+import org.aiwolf.common.data.Role;
+
+public class FirstCOMedium extends AbstractMedium {
+	private boolean isComingOut=false;
+	private ArrayList<Judge> myJudgeResult;
+	private ArrayList<Judge> toldJudgeResult=new ArrayList<Judge>();
+	
+	@Override
+	public void finish() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String talk() {
+		// TODO Auto-generated method stub
+		myJudgeResult=getMyJudgeList();
+		if(!isComingOut){
+			isComingOut=true;
+			return TemplateTalkFactory.comingout(getMe(), Role.MEDIUM);
+		}
+		else{
+			for(Judge judge: myJudgeResult){
+				if(!toldJudgeResult.contains(judge)){
+					toldJudgeResult.add(judge);
+					return TemplateTalkFactory.inquested(judge.getTarget(), judge.getResult());
+				}
+			}
+		}
+		return TemplateTalkFactory.over();
+	}
+
+	@Override
+	public Agent vote() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
