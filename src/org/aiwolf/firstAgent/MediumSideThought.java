e7b4e5dd97915aa0eef28c0ea37b6b3fdfb7cfa9
Haruhisa Ishida <haruhisa.ishida@gmail.com>
2015-07-04 13:51:45 +0900

    霊能結果をしゃべる処理を追加

--- src/org/aiwolf/firstAgent/MediumSideThought.java
+++ src/org/aiwolf/firstAgent/MediumSideThought.java
@@ -1,9 +1,21 @@
 package org.aiwolf.firstAgent;
 
 import org.aiwolf.client.base.player.AbstractMedium;
+import org.aiwolf.client.lib.TemplateTalkFactory;
 import org.aiwolf.common.data.Agent;
+import org.aiwolf.common.data.Judge;
+import org.aiwolf.common.data.Species;
+import org.aiwolf.common.net.GameInfo;
 
 public class MediumSideThought extends AbstractMedium {
+	boolean isAlreadyComingOuted = false;
+	boolean toldLatestJudge = false;
+
+	@Override
+	public void dayStart() {
+		super.dayStart();
+		toldLatestJudge = false;
+	}
 
 	@Override
 	public void finish() {
@@ -13,8 +25,28 @@ public class MediumSideThought extends AbstractMedium {
 
 	@Override
 	public String talk() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		GameInfo gameInfo = this.getLatestDayGameInfo();
+		Agent executedAgent = gameInfo.getExecutedAgent();
+		String result = null;
+		
+		if (!toldLatestJudge) {
+			for (Judge judge : getMyJudgeList()) {
+				if (executedAgent != null
+						&& executedAgent.compareTo(judge.getTarget()) == 0) {
+					switch (judge.getResult()) {
+					case HUMAN:
+					default:
+						result = TemplateTalkFactory.inquested(executedAgent, Species.HUMAN);
+						break;
+					case WEREWOLF:
+						result = TemplateTalkFactory.inquested(executedAgent, Species.WEREWOLF);
+						break;
+					}
+				}
+			}
+		}else
+			result = TemplateTalkFactory.over();
+		return result;
 	}
 
 	@Override
