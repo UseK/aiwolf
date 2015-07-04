00ca96f5b1943847a2ee0a52e40536924cdec019
Haruhisa Ishida <haruhisa.ishida@gmail.com>
2015-07-04 13:20:59 +0900

    霊能のクラスを追加．実装はしていない．

---
+++ src/org/aiwolf/firstAgent/MediumSideThought.java
@@ -0,0 +1,26 @@
+package org.aiwolf.firstAgent;
+
+import org.aiwolf.client.base.player.AbstractMedium;
+import org.aiwolf.common.data.Agent;
+
+public class MediumSideThought extends AbstractMedium {
+
+	@Override
+	public void finish() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public String talk() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	@Override
+	public Agent vote() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
