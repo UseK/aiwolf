0f128b0fe7a442dd55f08ac50255a98a3c57e2db
Haruhisa Ishida <haruhisa.ishida@gmail.com>
2015-07-04 13:07:08 +0900

    人狼のクラスを追加．実装はしていない．

---
+++ src/org/aiwolf/firstAgent/WolfSideThought.java
@@ -0,0 +1,44 @@
+package org.aiwolf.firstAgent;
+
+import org.aiwolf.client.base.player.AbstractWerewolf;
+import org.aiwolf.common.data.Agent;
+
+public class WolfSideThought extends AbstractWerewolf {
+
+	@Override
+	public Agent attack() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	@Override
+	public void dayStart() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void finish() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public String talk() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	@Override
+	public Agent vote() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	@Override
+	public String whisper() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
