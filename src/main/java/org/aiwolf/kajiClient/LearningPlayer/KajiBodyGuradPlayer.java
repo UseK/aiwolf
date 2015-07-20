44addefe293eecf2e6631c1fa7f25bb9bea87dbd
Glycine Bleumer <glycine@glycine-client.encode-fighter.luna.ddns.vc>
2015-07-20 12:41:06 +0900

    kajiAgentのinit commit
    http://www.aiwolf.org/sampleplayersource/ に公開されている
    kajiAgentをeclipseでbuildできるように整えたもの

---
+++ src/main/java/org/aiwolf/kajiClient/LearningPlayer/KajiBodyGuradPlayer.java
@@ -0,0 +1,66 @@
+package org.aiwolf.kajiClient.LearningPlayer;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.aiwolf.common.data.Agent;
+import org.aiwolf.common.data.Role;
+import org.aiwolf.kajiClient.lib.Pattern;
+import org.aiwolf.kajiClient.LearningPlayer.AbstractKajiBasePlayer;
+import org.aiwolf.kajiClient.reinforcementLearning.AgentPattern;
+import org.aiwolf.kajiClient.reinforcementLearning.Qvalues;
+import org.aiwolf.kajiClient.reinforcementLearning.ReinforcementLearning;
+import org.aiwolf.kajiClient.reinforcementLearning.Scene;
+
+public class KajiBodyGuradPlayer extends AbstractKajiBasePlayer {
+
+	@Override
+	public String getJudgeText() {
+		return null;
+	}
+
+	@Override
+	public String getComingoutText() {
+		return null;
+	}
+
+	@Override
+	public void setVoteTarget() {
+		setVoteTargetTemplate(myPatterns);
+	}
+
+	@Override
+	public Agent guard() {
+		Map<Agent, Double> agentPoint = new HashMap<Agent, Double>();
+		for(Agent agent: getLatestDayGameInfo().getAliveAgentList()){
+			double point = 0.0;
+			for(Pattern p: myPatterns){
+				Scene s = new Scene(p);
+				AgentPattern ap = p.getAgentPattern(agent);
+				Qvalues qVal = ld.getQvalue(s.getHashNum());
+				point += qVal.getHunterGuard().get(ap) * (double)qVal.getLikelihood();
+			}
+			agentPoint.put(agent, point);
+		}
+		return selectGreedyTarget(agentPoint);
+	}
+
+	@Override
+	void updateMiddlePattern(int day, Pattern patternPresent,
+			Pattern patternNext, Scene scenePresent, Scene sceneNext) {
+		super.updateMiddlePattern(day, patternPresent, patternNext, scenePresent, sceneNext);
+
+		//護衛の学習
+		Qvalues qVal = ld.getQvalue(scenePresent.getHashNum()),
+				qValNext = ld.getQvalue(sceneNext.getHashNum());
+		AgentPattern ap = patternPresent.getAgentPattern(getGameInfo(day).getGuardedAgent());
+		Map<AgentPattern, Double>
+				map = qVal.getHunterGuard(),
+				mapNext = qValNext.getHunterVote();
+		double nextMaxQVal = Qvalues.getMaxQValue(mapNext);
+		double learnedQ = ReinforcementLearning.reInforcementLearn(map.get(ap), 0.0, nextMaxQVal);
+		map.put(ap, learnedQ);
+	}
+
+}
---
+++ src/main/java/org/aiwolf/kajiClient/reinforcementLearning/AgentPattern.java
@@ -0,0 +1,42 @@
+package org.aiwolf.kajiClient.reinforcementLearning;
+
+import org.aiwolf.common.data.Agent;
+import org.aiwolf.kajiClient.lib.Pattern;
+
+public enum AgentPattern {
+	NULL,
+	
+	SEER,
+	
+	MEDIUM,
+	
+	FAKE_SEER_BLACK,
+	FAKE_SEER_WHITE,
+	FAKE_SEER_GRAY,
+	
+	FAKE_MEDIUM_BLACK,
+	FAKE_MEDIUM_WHITE,
+	FAKE_MEDIUM_GRAY,
+	
+	JUDGED_BLACK,
+	
+	WHITE_AGENT,
+	//襲撃されたエージェント
+	ATTACKED_AGENT,
+	//処刑されたエージェント
+	EXECUTED_AGENT,
+	
+	ORDINARY_AGENT;
+
+	public AgentPattern getAgentPattern(Pattern p, Agent a){
+		
+		if(a.equals(p.getSeerAgent())){
+			return SEER;
+		}else if(a.equals(p.getMediumAgent())){
+			return MEDIUM;
+		}
+		
+		return null;
+	}
+	
+}
