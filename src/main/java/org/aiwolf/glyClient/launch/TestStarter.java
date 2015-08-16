bed479eab51aeebf2f4214c206f6af3cd1385270
Haruhisa Ishida <haruhisa.ishida@gmail.com>
2015-08-16 14:01:13 +0900

    学習実行時のインスタンスIDを変更

--- src/main/java/org/aiwolf/glyClient/launch/TestStarter.java
+++ src/main/java/org/aiwolf/glyClient/launch/TestStarter.java
@@ -26,9 +26,10 @@ public class TestStarter {
 
 		// プレイヤーのインスタンスリスト
 		List<RoleAssignPlayer> players = new ArrayList<RoleAssignPlayer>(
-				PLAYER_NUM);
+				PLAYER_NUM + 1);
 
-		for (int i = 0; i < PLAYER_NUM; ++i){
+		// プレイヤーインスタンス自体は16個作らないと行けない
+		for (int i = 0; i <= PLAYER_NUM; ++i) {
 			LearningData.getInstance(i).LDStart();
 			RoleAssignPlayer player = new RoleAssignPlayer();
 			player.setLDNumber(i);
@@ -40,22 +41,22 @@ public class TestStarter {
 
 			/* configure playerlist in "playerMap" */
 			Map<Player, Role> playerMap = new HashMap<>();
-			
-			// ID: 0 は占い
-			playerMap.put(players.get(0), Role.SEER);
-			// ID: 1 は霊能
-			playerMap.put(players.get(1), Role.MEDIUM);
-			// ID: 2 は狩人
-			playerMap.put(players.get(2), Role.BODYGUARD);
-			// ID: 3 は狂人
-			playerMap.put(players.get(3), Role.POSSESSED);
-			
-			// ID: 4-6は人狼（15人なら3人のはず）
-			for( int i = 4; i <= 6; ++i )
+
+			// ID: 1 は占い
+			playerMap.put(players.get(1), Role.SEER);
+			// ID: 2 は霊能
+			playerMap.put(players.get(2), Role.MEDIUM);
+			// ID: 3 は狩人
+			playerMap.put(players.get(3), Role.BODYGUARD);
+			// ID: 4 は狂人
+			playerMap.put(players.get(4), Role.POSSESSED);
+
+			// ID: 5-7は人狼（レギュレーションより，3人のはず）
+			for (int i = 5; i <= 7; ++i)
 				playerMap.put(players.get(i), Role.WEREWOLF);
-			
-			// ID: 7-Lastは村人
-			for( int i = 7; i < PLAYER_NUM; ++i )
+
+			// ID: 8-Lastは村人
+			for (int i = 8; i <= PLAYER_NUM; ++i)
 				playerMap.put(players.get(i), Role.VILLAGER);
 
 			DirectConnectServer gameServer = new DirectConnectServer(playerMap);
@@ -74,7 +75,7 @@ public class TestStarter {
 		}
 
 		// 学習結果を保存する　
-		for (int i = 0; i < PLAYER_NUM; ++i)
+		for (int i = 1; i <= PLAYER_NUM; ++i)
 			LearningData.getInstance(i).LDFinish();
 
 		System.out.println("village: " + villageWinNum + ", werewolf: "
